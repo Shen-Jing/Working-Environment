6e2b77bd9cc736d2cb6ba7adc6cef29a70625e84
ShenJing <shenjingnerve@gmail.com>
2020-12-19 17:30:56 +0800

    [FEAT] the latest latex **interactive** script

--- Dockerfiles/Fedora/install_others.sh
+++ Dockerfiles/Fedora/install_others.sh
@@ -2,6 +2,14 @@
 
 # [Other]
 
+# Latex
+
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar zxvf install-tl-unx.tar.gz
+rm install-tl-unx.tar.gz
+cd $(ls -d install-tl*/)
+sudo perl install-tl
+
 # [Shell]
 
 # [Trivial]
